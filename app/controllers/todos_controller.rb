a2179fc71288cf70ff423b4b1dcfd64536c6d5e4
congdang <congdang@gmail.com>
2018-12-20 22:39:17 -0800

    make app production ready

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,6 @@
 class TodosController < ApplicationController
-	
+
+	before_action :set_todo, only: [:edit, :update,:show, :destroy]
 	def index
 		@todos = Todo.all
 	end
@@ -21,16 +22,35 @@ class TodosController < ApplicationController
 
 
 	def show
-		@todo = Todo.find(params[:id])
 	end
 
 
 	def edit
-		@todo = Todo.find(params[:id])
+	end
+
+	def update
+		if @todo.update(todo_params)
+			flash[:notice] = "Todo was successfully updated"
+			redirect_to todo_path(@todo)
+		else
+			render 'edit'
+		end
+	end
+
+
+	def destroy
+		@todo.destroy
+		flash[:notice] = "Todo was delete successfully"
+		redirect_to todos_path
 	end
 
 
 	private
+	def set_todo
+		@todo = Todo.find(params[:id])
+	end
+
+
 	def todo_params
 		params.require(:todo).permit(:name, :description)
 	end
